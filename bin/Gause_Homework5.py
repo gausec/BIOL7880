78b362a0aaec449ed9c1d05dfafbdd613c608173
Carol Gause <112657596+gausec@users.noreply.github.com>
2023-04-21 13:21:00 -0400

    Homework 5 Python3 Script

--- bin/Gause_Homework5.py
+++ bin/Gause_Homework5.py
@@ -1,16 +1,32 @@
 # Carol Gause
 # Homework 5
 
-# Specify the sequences to open from the command line
-homework5_sequences = input("Type name of sequence file here, please: ")
 
-# Create an outfile to save the sequences to
-homework5_out = open('homework5_out.txt', 'w')
-
-
-# Write the lines individually in the outfile
-with open(homework5_sequences, 'r') as HW5Seq:
-    for line in HW5Seq:
-        homework5_out.write(line)
+# Import sys module to allow access to command line arguments & set the variable 'file' to the first command line argument (name of input file)
+import sys
+file=sys.argv[1]
 
 
+# Open input file in read/write mode & create a new output file in read/write mode with the '.txt' extension
+fp1 = open(file, 'r')
+fp2 = open(file+'.txt', 'w+')
+
+
+# Read lines of the input file and store as a list of strings
+HW5seq=fp1.readlines()
+
+
+# Iterate through each line in the list of input file lines 
+    
+for line in HW5seq:
+    if line[0]=='>':                        # Write only the first element of the line (the sequence ID) followed by a new line if the first character is a '>'
+        fp2.write(line.split()[0]+'\n')
+    else:                                   # Otherwise write entire line to output file
+        fp2.write(line)
+
+        
+# Close the files        
+fp1.close()
+fp2.close()
+
+        
